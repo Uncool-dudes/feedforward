28b0af779e236cc08d8ca635f62b321c8540776e
Ananth Pullur <106897936+Uncool-dudes@users.noreply.github.com>
2025-10-18 19:33:25 +0530

    Added basic auth with clerk, currently using custom google o auth client credentials.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,14 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import {
+	ClerkProvider,
+	SignedIn,
+	SignedOut,
+	SignInButton,
+	SignUpButton,
+	UserButton,
+} from "@clerk/nextjs";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -24,13 +32,31 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
-				{children}
-				<Toaster />
-			</body>
-		</html>
+		<ClerkProvider>
+			<html lang="en">
+				<body
+					className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+				>
+					<header className="flex justify-end items-center p-4 gap-4 h-16">
+						<SignedOut>
+							<SignInButton />
+							<SignUpButton>
+								<button
+									className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer"
+									type="button"
+								>
+									Sign Up
+								</button>
+							</SignUpButton>
+						</SignedOut>
+						<SignedIn>
+							<UserButton />
+						</SignedIn>
+					</header>
+					{children}
+					<Toaster />
+				</body>
+			</html>
+		</ClerkProvider>
 	);
 }
