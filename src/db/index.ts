e34d2124601823330699c62597976e3d2a4c0ef2
Apoorv <130035517+APOORV7G@users.noreply.github.com>
2025-10-25 20:34:30 +0530

    Added Neon Functionality

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,12 +2,14 @@ import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "@/db/schema";
 import { env } from "@/env";
+import { Pool } from "pg";
+
+const pool = new Pool({
+  connectionString: env.DATABASE_URL,
+});
 
 export const db = drizzle({
-	connection: {
-		connectionString: env.DATABASE_URL,
-		ssl: false,
-	},
+	client: pool,
 	// logger: true,
 	casing: "snake_case",
 	schema,
